295f50f2e317188762751a164f344da64f028043
trevaokeefe <tokeefe@student.dwc.edu>
2015-01-16 14:52:57 -0500

    lab1

--- Lab1 - Treva/Lab1 - Treva/MyForm.h
+++ Lab1 - Treva/Lab1 - Treva/MyForm.h
@@ -34,9 +34,13 @@ namespace Project1 {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Button^  MichTech;
+	protected:
+
 	protected:
 	private: System::Windows::Forms::PictureBox^  pictureBox1;
+	private: System::Windows::Forms::Button^  NOmich;
+
 
 	private:
 		/// <summary>
@@ -51,36 +55,51 @@ namespace Project1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->button1 = (gcnew System::Windows::Forms::Button());
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
+			this->MichTech = (gcnew System::Windows::Forms::Button());
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->NOmich = (gcnew System::Windows::Forms::Button());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
-			// button1
+			// MichTech
 			// 
-			this->button1->Location = System::Drawing::Point(233, 288);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(202, 43);
-			this->button1->TabIndex = 0;
-			this->button1->Text = L"CLICK FOR AN AWESOME SCHOOL";
-			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			this->MichTech->Location = System::Drawing::Point(127, 288);
+			this->MichTech->Name = L"MichTech";
+			this->MichTech->Size = System::Drawing::Size(118, 43);
+			this->MichTech->TabIndex = 0;
+			this->MichTech->Text = L"CLICK FOR AN AWESOME SCHOOL";
+			this->MichTech->UseVisualStyleBackColor = true;
+			this->MichTech->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
 			// pictureBox1
 			// 
-			this->pictureBox1->Location = System::Drawing::Point(35, 29);
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(102, 22);
 			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(628, 243);
+			this->pictureBox1->Size = System::Drawing::Size(345, 243);
 			this->pictureBox1->TabIndex = 1;
 			this->pictureBox1->TabStop = false;
+			this->pictureBox1->Click += gcnew System::EventHandler(this, &MyForm::pictureBox1_Click);
+			// 
+			// NOmich
+			// 
+			this->NOmich->Location = System::Drawing::Point(305, 288);
+			this->NOmich->Name = L"NOmich";
+			this->NOmich->Size = System::Drawing::Size(118, 43);
+			this->NOmich->TabIndex = 2;
+			this->NOmich->Text = L"CLICK TO HIDE IT";
+			this->NOmich->UseVisualStyleBackColor = true;
+			this->NOmich->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(702, 343);
+			this->ClientSize = System::Drawing::Size(558, 343);
+			this->Controls->Add(this->NOmich);
 			this->Controls->Add(this->pictureBox1);
-			this->Controls->Add(this->button1);
+			this->Controls->Add(this->MichTech);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
@@ -88,8 +107,14 @@ namespace Project1 {
 
 		}
 #pragma endregion
-
-	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+	private: System::Void MichTech_Click(System::Object^  sender, System::EventArgs^  e) {
+				 pictureBox1->Image = true;
+	}
+	private: System::Void pictureBox1_Click(System::Object^  sender, System::EventArgs^  e) {
+				 
+	}
+	private: System::Void NOmich_Click(System::Object^  sender, System::EventArgs^  e) {
+				 pictureBox1->Image = false;
 	}
 	};
 }
